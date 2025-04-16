9c154a30677291233fd1b558ac6ba23fcf38938f
Nathan <nathan.pak.1@us.af.mil>
2025-04-16 12:00:17 -0500

    wow

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -21,8 +21,8 @@ require('dap-python').setup('python3')  -- Adjust path if needed
 -- You can map keys to start debugging or step through code
 vim.api.nvim_set_keymap('n', '<F5>', ':lua require"dap".continue()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F10>', ':lua require"dap".step_over()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F11>', ':lua require"dap".step_into()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<F12>', ':lua require"dap".step_out()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F9>', ':lua require"dap".step_into()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F8>', ':lua require"dap".step_out()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>b', ':lua require"dap".toggle_breakpoint()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>B', ':lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ui', ':lua require"dapui".toggle()<CR>', { noremap = true, silent = true })
@@ -52,7 +52,7 @@ table.insert(require('dap').configurations.python, {
 dap.adapters.c = {
     id = 'cppdbg',
     type = 'executable',
-    command = '/home/kali/.vscode/extensions/ms-vscode.cpptools-1.22.11-linux-x64/debugAdapters/bin/OpenDebugAD7',
+    command = '/home/nathan/.vscode-server/extensions/ms-vscode.cpptools-1.23.6-linux-x64/debugAdapters/bin/OpenDebugAD7',
 }
 
 local dap = require('dap')
